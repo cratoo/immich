a0c84a9e5b8e3c97e7549fb3c0f2f8010a24cb87
Andreas Heinz <github@a80.net>
2026-05-06 11:28:42 +0200

    add stacks to album view

--- web/src/routes/(user)/albums/[albumId=id]/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/albums/[albumId=id]/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -228,7 +228,7 @@
         timelineAlbumId: albumId,
       };
     }
-    return { albumId, order: album.order };
+    return { albumId, order: album.order, withStacked: true };
   });
 
   const isShared = $derived(viewMode === AlbumPageViewMode.SELECT_ASSETS ? false : album.albumUsers.length > 0);
